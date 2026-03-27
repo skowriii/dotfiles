2dc98111213a52ff9c1c3563acd2daa5744a5e14
Jakub Skowron <jakubskowron676@gmail.com>
2026-03-27 18:59:16 +0100

    feat(nvim): auto-install LSP's using mason-tool-installer

--- home/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ home/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,3 +1,21 @@
 MiniDeps.add({
-	source = "neovim/nvim-lspconfig"
+	source = "neovim/nvim-lspconfig",
+    depends = { "WhoIsSeth/mason-tool-installer.nvim" }
+})
+
+require("mason-tool-installer").setup({
+    ensure_installed = {
+        "bash-language-server",
+        "clangd",
+        -- "cmake-language-server",
+        "css-variables-language-server",
+        "html-lsp",
+        "hyprls",
+        "lua-language-server",
+        "python-lsp-server",
+        "qmlls",
+        "typescript-language-server"
+    },
+    auto_update = true,
+    debounce_hours = 8
 })
