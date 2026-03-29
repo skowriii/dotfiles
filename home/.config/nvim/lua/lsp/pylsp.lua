4978078afb97c89f31e2a4da8816f983f652c20b
Jakub Skowron <jakubskowron676@gmail.com>
2026-03-29 21:59:02 +0200

    feat(nvim/lsp/pylsp): disable mccabe and pycodestyle

--- home/.config/nvim/lua/lsp/pylsp.lua
+++ home/.config/nvim/lua/lsp/pylsp.lua
@@ -1,2 +1,15 @@
 require("user.utils").setup_capabilities("pylsp")
+
+vim.lsp.config("pylsp", {
+    settings = {
+        pylsp = {
+            plugins = {
+                mccabe = { enabled = false },
+                pycodestyle = { enabled = false },
+                pyflakes = { enabled = true }
+            }
+        }
+    }
+})
+
 vim.lsp.enable("pylsp")
