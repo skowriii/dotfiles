8243e408f4ceb17a35daa343a74bbcb772184fb3
Jakub Skowron <jakubskowron676@gmail.com>
2026-04-06 18:37:12 +0200

    feat(nvim): use built-in mini.icons instead of installing separately

--- home/.config/nvim/lua/smooll/plugins/file_explorers/oil.lua
+++ home/.config/nvim/lua/smooll/plugins/file_explorers/oil.lua
@@ -2,7 +2,6 @@ MiniMisc.safely("now",
     function()
         vim.pack.add({
             "https://github.com/stevearc/oil.nvim",
-            "https://github.com/nvim-mini/mini.icons"
         })
 
         require("oil").setup({
--- home/.config/nvim/lua/smooll/plugins/miscellaneous/render-markdown.lua
+++ home/.config/nvim/lua/smooll/plugins/miscellaneous/render-markdown.lua
@@ -1,7 +1,6 @@
 vim.pack.add({
     "https://github.com/MeanderingProgrammer/render-markdown.nvim",
     "https://github.com/nvim-treesitter/nvim-treesitter",
-    "https://github.com/echasnovski/nvim-mini/mini.icons"
 })
 
 require("render-markdown").setup({
