6979566c51836caf79bff5a2db2757e642bd3b63
Jakub Skowron <jakubskowron676@gmail.com>
2026-04-13 16:11:25 +0200

    feat(nvim/autocmd): add highlight_on_yank to init.lua

--- home/.config/nvim/lua/smooll/autocmd/init.lua
+++ home/.config/nvim/lua/smooll/autocmd/init.lua
@@ -1,6 +1,7 @@
 local modules = {
     "start_treesitter",
-    "update_colorcolumn"
+    "update_colorcolumn",
+    "highlight_on_yank"
 }
 
 require("smooll.user.utils").bulk_require("smooll.autocmd", modules)
