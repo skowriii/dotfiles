47b73682367e9b895fa7f875c93793d5be0eb502
Jakub Skowron <jakubskowron676@gmail.com>
2026-05-01 14:39:49 +0200

    feat: add JetBrainsMono Nerd Font as fallback font for nerd font icons

--- decman/fonts.py
+++ decman/fonts.py
@@ -8,11 +8,12 @@ class Fonts(Module):
     @pacman.packages
     def packages(self) -> set[str]:
         return {
+            "ttf-montserrat", # Primary GUI font
+            "ttf-jetbrains-mono-nerd" # Fallback for nerd font icons
             "noto-fonts",
             "noto-fonts-cjk",
             "noto-fonts-emoji",
             "noto-fonts-extra",
-            "ttf-montserrat" # Primary GUI font
         }
 
     @aur.packages
