5d780e4308200793b126083fdccf9cc5b6b4ce09
Jakub Skowron <jakubskowron676@gmail.com>
2026-05-04 20:18:26 +0200

    feat(hyprland/gamemode.sh): handle live wallpapers

--- home/.config/hypr/gamemode.sh
+++ home/.config/hypr/gamemode.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env sh
 
 HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR == 1 { print $2 }')
-if [ "$HYPRGAMEMODE" = 1 ]; then
+HYPRPWALLPID=$(pgrep -f -l "pwall" | grep bash | awk '{ print $1 }' 2>/dev/null)
+HYPRTMPFILE="/tmp/hyprgamemode.tmp"
+HYPRPWALLUSED=$(cat $HYPRTMPFILE 2>/dev/null) || 0
+HYPRPWALLUSED=$(($HYPRPWALLUSED))
+
+if [[ "$HYPRGAMEMODE" = 1 ]]; then
+    while kill -TERM $HYPRPWALLPID; do
+        awww kill
+
+        echo 1 > $HYPRTMPFILE
+    done
+
     hyprctl --batch "\
         keyword animations:enabled 0;\
         keyword animation borderangle,0; \
@@ -13,10 +24,21 @@ if [ "$HYPRGAMEMODE" = 1 ]; then
         keyword general:border_size 1;\
         keyword decoration:rounding 0"
     hyprctl notify 1 5000 "rgb(40a02b)" "Gamemode [ON]"
-    exit
+
+    exit 0
 else
+    if [[ $HYPRPWALLUSED -eq 1 ]]; then
+        waypaper --restore
+
+        pwall &
+        disown $!
+
+        rm $HYPRTMPFILE
+    fi
+
     hyprctl notify 1 5000 "rgb(d20f39)" "Gamemode [OFF]"
     hyprctl reload
+
     exit 0
 fi
 
