b0180d9544e23d969d6d43b344fb78e2dc8c1e06
Jakub Skowron <jakubskowron676@gmail.com>
2026-05-13 15:05:01 +0200

    feat(nvim/nvim-lspconfig): add "cmake-language-server" and "zls" to auto-install list

--- home/.config/nvim/lua/smooll/plugins/autocomplete/nvim-lspconfig.lua
+++ home/.config/nvim/lua/smooll/plugins/autocomplete/nvim-lspconfig.lua
@@ -7,14 +7,15 @@ require("mason-tool-installer").setup({
     ensure_installed = {
         "bash-language-server",
         "clangd",
-        -- "cmake-language-server",
+        "cmake-language-server",
         "css-variables-language-server",
         "html-lsp",
         "hyprls",
         "lua-language-server",
         "python-lsp-server",
         "qmlls",
-        "typescript-language-server"
+        "typescript-language-server",
+        "zls"
     },
     auto_update = true,
     debounce_hours = 8
