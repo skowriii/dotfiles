eb967075768b040b630df63a1802312281ff5fc5
Jakub Skowron <jakubskowron676@gmail.com>
2026-05-15 22:44:56 +0200

    refactor(hyprland/misc): change "0" to "false"

--- home/.config/hypr/land/misc.lua
+++ home/.config/hypr/land/misc.lua
@@ -1,6 +1,6 @@
 hl.config({
     misc = {
-        force_default_wallpaper = 0,
+        force_default_wallpaper = false,
         disable_hyprland_logo = true,
         vrr = true,
         render_unfocused_fps = 0
