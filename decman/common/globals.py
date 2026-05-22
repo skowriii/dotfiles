ed7a3dddd077854d6c710989f06ab4a5f121c492
Jakub Skowron <jakubskowron676@gmail.com>
2026-05-22 18:29:33 +0200

    feat(decman/common/globals): remove GPG keys for unused packages

--- decman/common/globals.py
+++ decman/common/globals.py
@@ -6,10 +6,7 @@ class Globals():
     terminal = "foot"
     user_manager = UserManager()
     gpg_receiver = GPGReceiver()
-    gpg_keys = [
-        "95D2E9AB8740D8046387FD151A09227B1F435A33", # bdf-unifont
-        "E1096BCBFF6D418796DE78515384CE82BA52C83A"  # Spotify
-    ]
+    gpg_keys = []
     root_directory = "/personal"
     dotfiles_directory = f"{root_directory}/dotfiles"
     nbfc_model = "HP ProBook 650 G2"
