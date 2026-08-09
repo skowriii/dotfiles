fee6b530f0836f4b0bd40ab85143d3f1ad13f535
Jakub Skowron <jakubskowron766@proton.me>
2026-08-09 19:43:03 +0200

    feat(decman/common/globals): add Spotify GPG key

--- decman/common/globals.py
+++ decman/common/globals.py
@@ -6,7 +6,9 @@ class Globals():
     terminal = "foot"
     user_manager = UserManager()
     gpg_receiver = GPGReceiver()
-    gpg_keys = []
+    gpg_keys = [
+        "E1096BCBFF6D418796DE78515384CE82BA52C83A" # spotify
+    ]
     root_directory = "/personal"
     dotfiles_directory = f"{root_directory}/dotfiles"
     nbfc_model = "HP ProBook 650 G2"
