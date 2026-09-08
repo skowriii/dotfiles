0fef19dbe9242216566786457aca80d3daae0e4e
Jakub Skowron <jakubskowron766@proton.me>
2026-09-08 18:18:27 +0200

    feat(hyprland): add hooks on hyprland.start/shutdown to fix not working XDPH

--- home/.config/hypr/hyprland.lua
+++ home/.config/hypr/hyprland.lua
@@ -2,4 +2,12 @@ hl.config({
     debug = { disable_logs = false }
 })
 
+hl.on("hyprland.start", function()
+	hl.exec_cmd("systemctl --user start hyprland-session.target")
+end)
+
+hl.on("hyprland.shutdown", function()
+	os.execute("systemctl --user stop hyprland-session.target && sleep 0.1")
+end)
+
 require("land")
